2d6bfad6d8d4d71afd50e3d1c228c2c3827a424f
EnzoLp <enzo.lpez.pstor@gmail.com>
2023-04-10 00:34:58 -0400

    version3

--- src/test/scala/Gwent/CardTest.scala
+++ src/test/scala/Gwent/CardTest.scala
@@ -1,6 +1,12 @@
 package cl.uchile.dcc
 package Gwent
 
-class CardTest {
+import munit.FunSuite
+class CardTest extends FunSuite{
+  val carta = new card("Clima Despejado")
+  test("Las cartas deben tener nombre"){
+    assertEquals(carta.nombre, "Clima Despejado")
+    
+  }
 
 }
--- src/test/scala/Gwent/PlayerTest.scala
+++ src/test/scala/Gwent/PlayerTest.scala
@@ -3,11 +3,20 @@ package Gwent
 
 import munit.FunSuite
 
-class GwentTest extends FunSuite{
+class PlayerTest extends FunSuite{
+  val player = new Jugador("Alejandro")
   test("Jugadores deben poseer nombre"){
-    val player = new Jugador("Alejandro")
-    assertEquals = 
-
+    assertEquals(player.nombre, "Alejandro")
+  }
+  test("Contador de Gemas tiene que ser igual o mayor a 0"){
+    assert(player.gemas>=0)
+  }
+  test("Cartas en la mano tiene que ser menor que 10 y mayor o igual que cero"){
+    assert(player.cartas >=0)
+    assert(player.cartas <10)
+  }
+  test("El mazo de cartas no puede tener menos de cero cartas"){
+    assert(player.mazo>0)
   }
 
 }
